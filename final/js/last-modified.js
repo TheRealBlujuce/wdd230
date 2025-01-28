54e387a056bf8a4518dcfd56000f717196b66dfd
WillMarda <WillMarda@outlook.com>
2025-01-28 10:31:56 -0700

    temp update

--- final/js/last-modified.js
+++
@@ -1,4 +0,0 @@
-const lastModifiedDate = new Date(document.lastModified);
-const lastModified = document.getElementById("modified");
-console.log(lastModified)
-lastModified.innerHTML = `Last updated: ${lastModifiedDate.toLocaleDateString()} ${lastModifiedDate.toLocaleTimeString()}`;
